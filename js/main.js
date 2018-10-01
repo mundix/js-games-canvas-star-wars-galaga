8a846907ead145cf1ccfaba4cef4da606a118af8
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-01 11:37:07 -0400

    add crafty lib

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-$(function(){
-    init();
+// $(function(){
+    // init();
+    // var Crafty = require('craftyjs');
+// Crafty("screen");
+Crafty.init(900,900,"screen");
+
+Crafty.background('black');
+
+// Crafty.e('2D, Canvas, Color, Fourway')
+//   .attr({x: 10, y: 10, w: 30, h: 30})
+//   .color('red')
+//   .fourway(3);
+// });
+var square = Crafty.e('2D, Canvas, Color');
+square.attr({
+  x: 10,
+  y: 10,
+  w: 100,
+  h: 100
+}).color('red');
+
+square.origin("center");
+
+square.bind('UpdateFrame', function(){
+  this.rotation = this.rotation + 1;
 });
+
+
 
 
 

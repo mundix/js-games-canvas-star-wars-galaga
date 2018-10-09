0aa1a2d7562323e89924a02071b0807dc4a3ba7c
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-09 12:25:36 -0400

    rebasing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 var canvas = document.getElementById("screen");
 var context = canvas.getContext("2d");
 
-canvas.width = 256;
-canvas.height = 256;
+canvas.width = 256*2;
+canvas.height = 256*2;
+
+context.beginPath(); 
+context.lineWidth="5";
+context.strokeStyle="green"; // Green path
+context.moveTo(0,5);
+context.lineTo(100,5);
+context.stroke(); // Draw it
+
+context.beginPath(); 
+context.lineWidth="5";
+context.strokeStyle="red"; // Green path
+context.moveTo(0,10);
+context.lineTo(50,10);
+context.stroke(); // Draw it
+
+var X = 200;
+var Y = 200;
+var W = 100;
+var H = 100;
+
+// context.fillStyle = 'red';
+// context.fillRect(64, 64, 128, 128); //Middle square.
+context.drawImage(document.getElementById("ship"), X, Y, W, H);
 
 context.save(); //Save transformations.
-
 //Translate, rotate.
-context.translate(128,128);
-context.rotate(25*Math.PI/180);
-context.translate(-128,-128);
+var X = 200;
+var Y = 200;
+var W = 100;
+var H = 100;
+context.translate(X,Y);
+context.rotate(45*Math.PI/180);
+context.translate(-X,-Y);
 
-context.fillRect(64, 64, 128, 128); //Middle square.
+context.drawImage(document.getElementById("ship"), X, Y, W, H);
 
+// context.drawImage(document.getElementById("ship"), 50, 50, 100, 100);
 context.restore(); //Restore default transformations.
 
-context.fillRect(2, 2, 16, 16); //Corner square.
+// $(function(){
+//     init();
+// });
+
